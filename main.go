bc61a9e01e386d1b241ee9f76d01d65ac765b8d1
Shubham Bind <2shubhambind@gmail.com>
2026-03-24 20:22:11 +0530

    added concurrency

--- main.go
+++ main.go
@@ -2,79 +2,122 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"strings"
+	"sync"
 
 	"golang.org/x/net/html"
 )
 
-var client = &http.Client{}
-var filename = "links.txt"
-var visited = make(map[string]bool)
-var base = "https://www.cloudflare.com/learning/bots/what-is-a-web-crawler/"
+var fileMu sync.Mutex
 
-func crawl(queue []string) {
+var mu sync.Mutex
+var client = &http.Client{}
+var visited = make(map[string]bool)
+var base = "https://en.wikibooks.org/"
+var filename = "links.txt"
+
+func crawl(queue chan string, wg *sync.WaitGroup, file *os.File) {
+
+	for link := range queue {
+		func() {
+			defer wg.Done()
+			if strings.HasPrefix(link, "javascript:") ||
+				strings.HasPrefix(link, "mailto:") ||
+				strings.HasPrefix(link, "tel:") {
+				return
+			}
+			req, err := http.NewRequest("GET", link, nil)
+			if err != nil {
+				return
+			}
+			req.Header.Set("User-Agent", "Mozilla/5.0 (compatible; situp-bot/1.0)")
+			resp, err := client.Do(req)
+			if err != nil {
+				return
+			}
+			if resp.StatusCode != http.StatusOK {
+				resp.Body.Close()
+				return
+			}
+			tokenizer := html.NewTokenizer(resp.Body)
+
+		tokenizerLoop:
+			for {
+				tokentype := tokenizer.Next()
+				switch tokentype {
+				case html.ErrorToken:
+					break tokenizerLoop
+				case html.StartTagToken, html.SelfClosingTagToken:
+					token := tokenizer.Token()
+					if token.Data == "a" {
+						for _, attr := range token.Attr {
+							if attr.Key == "href" {
+								link := attr.Val
+								if len(link) > 0 && link[0] == '#' {
+									continue
+								}
+								if len(link) > 0 && link[0] == '/' {
+									link = base + link
+								}
+								mu.Lock()
+								if visited[link] {
+									mu.Unlock()
+									continue
+
+								}
+								visited[link] = true
+								mu.Unlock()
+
+								select {
+								case queue <- link:
+									// success
+									wg.Add(1)
+								default:
+									// queue full → drop or skip
+								}
+
+								fileMu.Lock()
+								if _, err := fmt.Fprintln(file, link); err != nil {
+									fileMu.Unlock()
+									continue
+								}
+								fileMu.Unlock()
+							}
+						}
+					}
+				}
+			}
+
+			resp.Body.Close()
+		}()
+	}
+
+}
+func main() {
+	c := make(chan string, 1000)
 	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return
 	}
 	defer file.Close()
-	for len(queue) > 0 {
-		link := queue[0]
-		if len(link) > 0 && link[0] == '/' {
-			link = base + link
-		}
-		queue = queue[1:]
-		req, err := http.NewRequest("GET", link, nil)
-		if err != nil {
-			continue
-		}
-		req.Header.Set("User-Agent", "Mozilla/5.0 (compatible; situp-bot/1.0)")
-
-		resp, err := client.Do(req)
-		if err != nil {
-			continue
-		}
-
-		tokenizer := html.NewTokenizer(resp.Body)
-	TokenizerLoop:
-		for {
-			tokenType := tokenizer.Next()
-
-			switch tokenType {
-			case html.ErrorToken:
-				// End of the document
-				break TokenizerLoop
-			case html.StartTagToken, html.SelfClosingTagToken:
-				token := tokenizer.Token()
-				// Check if the tag is an anchor tag "a"
-				if token.Data == "a" {
-					for _, attr := range token.Attr {
-						if attr.Key == "href" {
-							link := attr.Val
-							if len(link) > 0 && link[0] == '/' {
-								link = base + link
-							}
-							if visited[link] {
-								continue
-							}
-							queue = append(queue, link)
-							visited[link] = true
-							if _, err := fmt.Fprintln(file, link); err != nil {
-								continue
-							}
-
-						}
-					}
-				}
-			}
-		}
-		resp.Body.Close()
+	if err != nil {
+		log.Fatal(err)
 	}
+	link := "https://en.wikibooks.org/wiki/Department:Engineering"
+	var wg sync.WaitGroup
+	wg.Add(1)
+	c <- link
+	for i := 0; i < 10; i++ {
+		go crawl(c, &wg, file)
+	}
+	go func() {
+		wg.Wait()
+		close(c)
+	}()
+	wg.Wait()
+	fmt.Println("Done")
 
-}
-func main() {
-	//c := make(chan string)
-	queue := []string{"https://en.wikiversity.org/wiki/Wikiversity:Main_Page"}
-	crawl(queue)
 }
